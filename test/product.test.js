89e4d6fac00c2cedec216b5d084428f740cb6aad
WSL_SCD <49774633+Acid-Free@users.noreply.github.com>
2023-03-02 12:51:48 +0800

    Add chai http

--- test/product.test.js
+++ test/product.test.js
@@ -2,9 +2,14 @@ process.env.NODE_ENV = "test";
 
 const chai = require("chai");
 const expect = chai.expect;
+const should = chai.should();
+const chaiHttp = require("chai-http");
+const server = require("../server");
 
-describe("/First Test Collection", function () {
-  it("should test two values...", function () {
+chai.use(chaiHttp);
+
+describe("/First Test Collection", () => {
+  it("should test two values...", () => {
     // actual test content in here
     let expectedValue = 10;
     let actualValue = 5;
