27dfbf2acb280c11ccac33eafee481daf63c054c
WSL_SCD <49774633+Acid-Free@users.noreply.github.com>
2023-03-02 14:47:09 +0800

    Add database testing

--- test/product.test.js
+++ test/product.test.js
@@ -1,5 +1,6 @@
 process.env.NODE_ENV = "test";
 
+const Product = require("../models/product");
 const chai = require("chai");
 const expect = chai.expect;
 const should = chai.should();
@@ -7,6 +8,16 @@ const chaiHttp = require("chai-http");
 const server = require("../server");
 
 chai.use(chaiHttp);
+
+before((done) => {
+  Product.deleteMany({}, (err) => {});
+  done();
+});
+
+after((done) => {
+  Product.deleteMany({}, (err) => {});
+  done();
+});
 
 describe("/First Test Collection", () => {
   it("test default API welcome route...", (done) => {
@@ -19,18 +30,58 @@ describe("/First Test Collection", () => {
         response.body.should.be.a("object");
 
         const actualValue = response.body.message;
-
         expect(actualValue).to.be.equal("Welcome to the MEN-REST-API");
 
         done();
       });
   });
 
-  it("should test two values...", () => {
-    // actual test content in here
-    let expectedValue = 10;
-    let actualValue = 10;
+  it("should verify that we have 0 products in the DB", (done) => {
+    chai
+      .request(server)
+      .get("/api/products")
+      .end((error, response) => {
+        response.should.have.status(200);
 
-    expect(actualValue).to.be.equal(expectedValue);
+        response.body.should.be.a("array");
+
+        response.body.length.should.be.equal(0);
+
+        done();
+      });
+  });
+
+  it("should POST a valid product", (done) => {
+    let product = {
+      name: "Test Product 1",
+      description: "Test product 1 Description",
+      price: 100,
+      inStock: true,
+    };
+
+    chai
+      .request(server)
+      .post("/api/products")
+      .send(product)
+      .end((error, response) => {
+        response.should.have.status(201);
+
+        done();
+      });
+  });
+
+  it("should verify that we have 1 product in the DB", (done) => {
+    chai
+      .request(server)
+      .get("/api/products")
+      .end((error, response) => {
+        response.should.have.status(200);
+
+        response.body.should.be.a("array");
+
+        response.body.length.should.be.equal(1);
+
+        done();
+      });
   });
 });
