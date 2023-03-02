f850d308bab5ba36cc97dd0549895db039f9d8b0
WSL_SCD <49774633+Acid-Free@users.noreply.github.com>
2023-03-02 13:37:45 +0800

    Add welcome route test

--- test/product.test.js
+++ test/product.test.js
@@ -9,10 +9,27 @@ const server = require("../server");
 chai.use(chaiHttp);
 
 describe("/First Test Collection", () => {
+  it("test default API welcome route...", (done) => {
+    chai
+      .request(server)
+      .get("/api/welcome")
+      .end((error, response) => {
+        response.should.have.status(200);
+
+        response.body.should.be.a("object");
+
+        const actualValue = response.body.message;
+
+        expect(actualValue).to.be.equal("Welcome to the MEN-REST-API");
+
+        done();
+      });
+  });
+
   it("should test two values...", () => {
     // actual test content in here
     let expectedValue = 10;
-    let actualValue = 5;
+    let actualValue = 10;
 
     expect(actualValue).to.be.equal(expectedValue);
   });
